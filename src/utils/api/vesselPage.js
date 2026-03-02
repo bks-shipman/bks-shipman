72c1c777da2e17bc1c8571f03f2688acdabf18ca
aryooo11 <machfudzaryo@gmail.com>
2026-03-02 11:34:31 +0700

    staging err

--- src/utils/api/vesselPage.js
+++ src/utils/api/vesselPage.js
@@ -2,7 +2,13 @@ import axiosInstance from "../axios";
 
 // Ambil semua SIJ
 export async function getVesselPage() {
-    const res = await axiosInstance.get(`/vessel-page`);
-    return res.data;
+    try {
+        const res = await axiosInstance.get(`/vessel-page`);
+        console.log("VESSEL DATA:", res.data);
+        return res.data;
+    } catch (err) {
+        console.log("ERROR VESSEL:", err.response?.data);
+        return null;
+    }
 }
 
